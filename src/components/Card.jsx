c5c7db619a3ba2d88c9be153f15a293432e84952
Jonh-ratana <ratanayorm787@gmail.com>
2025-03-02 09:07:18 +0700

    a

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,43 +2,43 @@ export default function Card(){
     const image = [
         {
             title : "",
-            image : "public/drink1.png",
+            image : "public/img/drink1.png",
         },
         {
             title : "",
-            image : "public/drink2.png",
+            image : "public/img/drink2.png",
         },
         {
             title : "",
-            image : "public/drink3.png",
+            image : "public/img/drink3.png",
         },
         {
             title : "",
-            image : "public/drink1.png",
+            image : "public/img/drink1.png",
         },
         {
             title : "",
-            image : "public/drink1.png",
+            image : "public/img/drink1.png",
         },
         {
             title : "",
-            image : "public/drink1.png",
+            image : "public/img/drink1.png",
         },
         {
             title : "",
-            image : "src/assets/drink2.png",
+            image : "public/img/drink2.png",
         },
         {
             title : "",
-            image : "src/assets/drink3.png",
+            image : "public/img/drink3.png",
         },
         {
             title : "",
-            image : "public/drink1.png",
+            image : "public/img/drink1.png",
         },
         {
             title : "",
-            image : "public/drink1.png",
+            image : "public/img/drink1.png",
         },
     ]
     return(
